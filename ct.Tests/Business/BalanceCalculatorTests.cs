9f73cc2176e6666d9aca81ebee88b1d03980c069
Jacob <wjmoody03@yahoo.com>
2013-06-05 22:32:01 -0700

    Added test class

---
+++ ct.Tests/Business/BalanceCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ct.Tests.Business
+{
+    [TestClass]
+    public class BalanceCalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
