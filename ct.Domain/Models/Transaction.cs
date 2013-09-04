0471197d4f70af801f23770b7bdf1b874f45d318
Jacob <wjmoody03@yahoo.com>
2013-09-03 23:31:06 -0700

    Added basic API controller for transaction

--- ct.Domain/Models/Transaction.cs
+++ ct.Domain/Models/Transaction.cs
@@ -9,10 +9,12 @@ namespace ct.Domain.Models
 {
     public class Transaction
     {
+        [Column("ID")]
         public int TransactionID { get; set; }
+        [Column("TransactionType")]
         public int TransactionTypeID { get; set; }
         public int AccountID { get; set; }
-        public int DownloadID { get; set; }
+        public int? DownloadID { get; set; }
         public string SourceTransactionIdentifier { get; set; } //this is the transID of the bank or card company
         public int? ParentTransactionID { get; set; }
         public DateTime TransactionDate { get; set; }
@@ -23,12 +25,14 @@ namespace ct.Domain.Models
         public string ReimbursableSource { get; set; }
         public string TaxTag { get; set; }
         public bool FlagForFollowUp { get; set; }
-        [ForeignKey("TransactionID")]
-        public List<Transaction> SplitTransactions { get; set; }
+        
+        [ForeignKey("ParentTransactionID")]
+        public ICollection<Transaction> SplitTransactions { get; set; }
 
         public TransactionType TransactionType { get; set; }
         public Account Account { get; set; }
         public Download Download { get; set; }
+        [ForeignKey("TransactionID")]
         public Transaction ParentTransaction { get; set; }
 
 
