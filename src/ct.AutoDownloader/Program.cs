2246f60cf6bb7e96766da45371eaa7415eca595a
Jacob Moody <jacob@SlickETL.com>
2015-09-01 21:23:04 -0700

    Added some code to the azure webjob

--- src/ct.AutoDownloader/Program.cs
+++ src/ct.AutoDownloader/Program.cs
@@ -4,19 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using ct.Business;
+using ct.Data.Repositories;
+using Newtonsoft.Json;
+using ct.Data.Contexts;
+using ct.Domain;
 
 namespace ct.AutoDownloader
 {
     // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
     class Program
     {
+        private static IctContext cxt;
+        private static IAccountRepository acctRepo;
+        private static ITransactionRepository transRepo;
+
         // Please set the following connection strings in app.config for this WebJob to run:
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
+            cxt = new ctContext(CashTrackerConfigurationManager.AzureSQLConnectionString);
+            acctRepo = new AccountRepository(cxt);
+            transRepo = new TransactionRepository(cxt);
+            var acct = acctRepo.FindBy(a => a.AccountID == 24).FirstOrDefault(); //chase account
+            var ccd = new CreditCardTransactionDownloader(acct, transRepo);
+            var adr = ccd.GetAllTransactions();
+            acct.LastImport = DateTime.Now;
+            acct.StatedBalanceAtInstitution = adr.AccountBalance;
+            acctRepo.Edit(acct);
+            acctRepo.Save();
+            transRepo.AddRange(adr.NewTransactions);
+            transRepo.Save();
         }
     }
 }
